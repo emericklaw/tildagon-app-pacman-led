3b709b2e338a2992d1578fed28c164b6f4603253
Matt Emerick-Law <matt@emericklaw.co.uk>
2024-06-27 11:25:51 +0100

    Update pin configuration for FW 1.9.0

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from app_components import Menu, Notification, clear_background
 import machine
 import utime
 from tildagon import Pin as ePin
+import settings
+from system.hexpansion.config import HexpansionConfig
+from app_components import layout as layout
 
 main_menu_items = [
     "Power",
+    "Slot",
+    "About",
 ]
 
 power_menu_items = ["On", "Off"]
@@ -23,16 +28,42 @@ class Pacman(app.App):
 
         # Main app setup
         self.power = True
-        self.dot1 = ePin("2_LS_D", machine.Pin.OUT)
-        self.dot2 = ePin("2_LS_C", machine.Pin.OUT)
-        self.dot3 = ePin("2_LS_B", machine.Pin.OUT)
-        self.eye = ePin("2_LS_A", machine.Pin.OUT)
-        self.dot1.value(1)
-        self.dot2.value(1)
-        self.dot3.value(1)
-        self.eye.value(0)
+
+        self.pins = {
+            "eye": None,
+            "dot_1": None,
+            "dot_2": None,
+            "dot_3": None,
+        }
+
+        self._init_pins()
         self.frame = 1
         self.last_update = utime.ticks_ms()
+
+    def _init_pins(self):
+        self.hexpansion_config = HexpansionConfig(settings.get("pacmanled.slot", 1))
+        self.pins["eye"] = self.hexpansion_config.ls_pin[0]
+        self.pins["eye"].init(self.pins["eye"].OUT)
+        self.pins["dot_1"] = self.hexpansion_config.ls_pin[3]
+        self.pins["dot_1"].init(self.pins["dot_1"].OUT)
+        self.pins["dot_2"] = self.hexpansion_config.ls_pin[2]
+        self.pins["dot_2"].init(self.pins["dot_2"].OUT)
+        self.pins["dot_3"] = self.hexpansion_config.ls_pin[1]
+        self.pins["dot_3"].init(self.pins["dot_3"].OUT)
+        self.pins["dot_1"].value(1)
+        self.pins["dot_2"].value(1)
+        self.pins["dot_3"].value(1)
+        self.pins["eye"].value(0)
+
+    def set_slot(self, slot):
+        slot = int(slot)
+        settings.set("pacmanled.slot", slot)
+        settings.save()
+        self.pins["dot_1"].value(0)
+        self.pins["dot_2"].value(0)
+        self.pins["dot_3"].value(0)
+        self.pins["eye"].value(0)
+        self._init_pins()
 
     def back_handler(self):
         # If in the topmost menu, minimize, otherwise move one menu up.
@@ -50,15 +81,20 @@ class Pacman(app.App):
                 if item == "On":
                     self.notification = Notification("Power On")
                     self.power = True
-                    self.eye.value(0)
+                    self.pins["eye"].value(0)
                     self.set_menu("main")
                 elif item == "Off":
                     self.notification = Notification("Power Off")
                     self.power = False
-                    self.dot1.value(1)
-                    self.dot2.value(1)
-                    self.dot3.value(1)
-                    self.eye.value(1)
+                    self.pins["dot_1"].value(1)
+                    self.pins["dot_2"].value(1)
+                    self.pins["dot_3"].value(1)
+                    self.pins["eye"].value(1)
+                    self.set_menu("main")
+            elif self.current_menu == "Slot":
+                if item in ["1", "2", "3", "4", "5", "6"]:
+                    self.notification = Notification("Slot=" + item)
+                    self.set_slot(item)
                     self.set_menu("main")
             else:
                 self.notification = Notification(self.current_menu + "." + item + '"!')
@@ -93,6 +129,22 @@ class Pacman(app.App):
                 back_handler=self.back_handler,
                 position=0 if self.power else 1,
             )
+        elif menu_name == "Slot":
+            self.menu = Menu(
+                self,
+                ["1", "2", "3", "4", "5", "6"],
+                select_handler=self.select_handler,
+                back_handler=self.back_handler,
+                position=settings.get("pacmanled.slot", 1) - 1,
+            )
+        elif menu_name == "About":
+            self.menu = Menu(
+                self,
+                [
+                    "",
+                ],
+                back_handler=self.back_handler,
+            )
 
     def draw(self, ctx):
         clear_background(ctx)
@@ -101,6 +153,23 @@ class Pacman(app.App):
 
         if self.notification:
             self.notification.draw(ctx)
+
+        if self.current_menu == "About":
+            # ctx.font_size = 24
+            ctx.save()
+            self.about_layout = layout.LinearLayout(
+                [
+                    layout.TextDisplay("- Version: 0.1.0"),
+                    layout.TextDisplay(
+                        "Control the LEDs on the Pacman hexpansion created by The Untitled Goose"
+                    ),
+                ]
+            )
+            self.about_layout.y_offset = 65
+            self.about_layout.x_offset = -60
+            self.about_layout.draw(ctx)
+
+            ctx.restore()
 
     def update(self, delta):
         self.menu.update(delta)
@@ -113,21 +182,21 @@ class Pacman(app.App):
             if utime.ticks_diff(current_time, self.last_update) >= 500:
                 self.last_update = current_time
                 if self.frame == 1:
-                    self.dot1.value(1)
-                    self.dot2.value(1)
-                    self.dot3.value(0)
+                    self.pins["dot_1"].value(1)
+                    self.pins["dot_2"].value(1)
+                    self.pins["dot_3"].value(0)
                 if self.frame == 2:
-                    self.dot1.value(1)
-                    self.dot2.value(0)
-                    self.dot3.value(1)
+                    self.pins["dot_1"].value(1)
+                    self.pins["dot_2"].value(0)
+                    self.pins["dot_3"].value(1)
                 if self.frame == 3:
-                    self.dot1.value(0)
-                    self.dot2.value(1)
-                    self.dot3.value(1)
+                    self.pins["dot_1"].value(0)
+                    self.pins["dot_2"].value(1)
+                    self.pins["dot_3"].value(1)
                 if self.frame == 4:
-                    self.dot1.value(1)
-                    self.dot2.value(1)
-                    self.dot3.value(1)
+                    self.pins["dot_1"].value(1)
+                    self.pins["dot_2"].value(1)
+                    self.pins["dot_3"].value(1)
                 self.frame = self.frame + 1
                 if self.frame > 4:
                     self.frame = 1
